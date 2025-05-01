186449689ec1f20ff601d069d2eb95bbf1a8d8ca
gesifeta <adamgemechu@gmail.com>
2025-05-01 17:30:53 +0300

    updated image url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://localhost:8000/api";
-export const IMAGE_URL = "http://localhost:8000";
+// export const API_URL = "http://localhost:8000/api";
+// export const IMAGE_URL = "http://localhost:8000";
 
-// export const API_URL = "https://gemechuadam-backend.netlify.app/api";
-// export const IMAGE_URL = "https://gemechuadam-backend.netlify.app";
+export const API_URL = "https://gemechuadam-backend.netlify.app/api";
+export const IMAGE_URL = "https://gemechuadam-backend.netlify.app";
